21803e4c8d5944aaeccc153594922c403a6e299e
Joaquin Ponte <joaquin.ponte@gmail.com>
2020-09-23 01:59:26 -0400

    Removed unused imports

--- src/main/java/com/jpworks/datajdbc/employee/controller/EmployeeController.java
+++ src/main/java/com/jpworks/datajdbc/employee/controller/EmployeeController.java
@@ -5,8 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
